23dc2424724ef60a828fa687aeeffcc228e5df64
Afik Tahirov <afik.t38@gmail.com>
2023-09-23 03:16:04 +0400

    Header logo add

---
+++ src/components/Hero.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Hero = () => {
+return (
+<section
+    className='lg:h-[85vh] flex items-center 
+    bg-primary lg:bg-cover lb:bg-center 
+    lg:bg-no-repeat py-32 lg:py-0 overflow-hidden '>
+    <div className='container mx-auto '>hero</div>
+</section>
+)
+}
+
+export default Hero
