6856cb6c79b740126db55272dac3b451daf66f71
Afik Tahirov <afik.t38@gmail.com>
2023-09-24 14:47:20 +0400

    Added skills page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header';
 import Hero from './components/Hero';
 import Brands from './components/Brands';
 import About from './components/About';
+import Skills from './components/Skills';
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
       <Hero/>
       <Brands />
       <About />
+      <Skills/>
       <div style={{height:'2000px'}}></div>
     </div>
   )
