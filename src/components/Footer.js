1678ce22e7610fd546403feef4a3b0be93e78826
Afik Tahirov <afik.t38@gmail.com>
2023-09-28 00:31:46 +0400

    Footer updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,31 @@
 import React from 'react'
+import { social } from '../data'
 
+import Logo from '../assets/img/logo3.svg'
 const Footer = () => {
   return (
-    <div>Footer</div>
+   <footer className='bg-tertiary py-12'>
+      <div className='container mx-auto'>
+        <div className='flex flex-col lg:flex-row space-y-6 lg:space-y-0 items-center justify-between'>
+            <div className='flex space-x-6 items-center justify-center'>
+                {social.map((item,index)=>{
+                    const {href,icon} = item;
+                    return( 
+                    <a className='text-accent text-base'  
+                    href={href} key={index}>
+                        {icon}
+                    </a>
+                    )
+                })}
+            </div>
+
+            <div>
+                <img src={Logo} alt=''/>
+            </div>
+            <p>&copy; 2023 Afik Tahirov. All rights reserved.</p>
+        </div>
+      </div>
+   </footer>
   )
 }
 
